3df92ef5640dfea68f8cf5e9eea23379db8bc666
Petr Liszok <liszok@email.cz>
2023-03-01 11:57:14 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void printAllIntegersFromList(List<Integer> list) {
+        int input = Support.safeReadInt();
+        new ArrayList<>();
+        while (input >= 0) {
+            System.out.println("You entered: " + input);
+            list.add(input);
+            System.out.println("Enter another integer:");
+            input = Support.safeReadInt();
+        }
+        if (input < 0) {
+            System.out.println("You entered negative");
+        }
+        for (Integer integer : list)
+            System.out.println(input);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("--- Task 4 - print all integers from list ---");
+        printAllIntegersFromList(list);
+
+    }
 }
